fc2b841b016dc17fe9143b6bcae53d4cdc4b319c
Tategoto Azarasi <2724167997@qq.com>
2025-03-13 21:37:26 +0800

    week6
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -7,8 +7,40 @@ where
     T: Send + 'static,
     U: Send + 'static + Default,
 {
-    let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    let len = input_vec.len();
+    let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len()); // 初始化输出向量
+    for i in 0..len {
+        output_vec.push(U::default());
+    }
+
+    // 创建通道：发送任务和接收结果
+    let (task_sender, task_receiver) = crossbeam_channel::unbounded::<(usize, T)>();
+    let (result_sender, result_receiver) = crossbeam_channel::unbounded::<(usize, U)>();
+
+    // 启动工作线程
+    for _ in 0..num_threads {
+        let task_receiver = task_receiver.clone();
+        let result_sender = result_sender.clone();
+        thread::spawn(move || {
+            while let Ok((index, input)) = task_receiver.recv() {
+                let output = f(input); // 执行 f
+                result_sender.send((index, output)).unwrap();
+            }
+        });
+    }
+
+    // 分发任务
+    for (i, input) in input_vec.into_iter().enumerate() {
+        task_sender.send((i, input)).unwrap();
+    }
+    drop(task_sender); // 关闭任务发送端，确保线程知道没有更多任务
+
+    // 收集结果
+    for _ in 0..len {
+        let (index, output) = result_receiver.recv().unwrap();
+        output_vec[index] = output;
+    }
+
     output_vec
 }
 
