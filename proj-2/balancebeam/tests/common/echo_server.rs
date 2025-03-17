1b57bbd61d96c768ca00e97a09eb92285e2ab91b
Tategoto Azarasi <2724167997@qq.com>
2025-03-17 20:07:03 +0800

    modernize balancebeam skeleton code
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- proj-2/balancebeam/tests/common/echo_server.rs
+++ proj-2/balancebeam/tests/common/echo_server.rs
@@ -1,10 +1,14 @@
 use crate::common::server::Server;
 use async_trait::async_trait;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response};
+use http_body_util::BodyExt;
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
 use rand::Rng;
 use std::sync::{atomic, Arc};
 use tokio::sync::oneshot;
+// Required for collect and to_bytes
 
 #[derive(Debug)]
 struct ServerState {
@@ -13,8 +17,8 @@ struct ServerState {
 
 async fn echo(
     server_state: Arc<ServerState>,
-    req: Request<Body>,
-) -> Result<Response<Body>, hyper::Error> {
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, hyper::Error> {
     server_state
         .requests_received
         .fetch_add(1, atomic::Ordering::SeqCst);
@@ -28,8 +32,9 @@ async fn echo(
     }
     req_text += "\n";
     let mut req_as_bytes = req_text.into_bytes();
-    req_as_bytes.extend(hyper::body::to_bytes(req.into_body()).await?);
-    Ok(Response::new(Body::from(req_as_bytes)))
+    let body_bytes = req.into_body().collect().await?.to_bytes(); // Correctly collect body into Bytes
+    req_as_bytes.extend(body_bytes);
+    Ok(Response::new(Full::new(Bytes::from(req_as_bytes))))
 }
 
 pub struct EchoServer {
@@ -41,46 +46,53 @@ pub struct EchoServer {
 
 impl EchoServer {
     pub async fn new() -> EchoServer {
-        let mut rng = rand::thread_rng();
-        EchoServer::new_at_address(format!("127.0.0.1:{}", rng.gen_range(1024, 65535))).await
+        let mut rng = rand::rng();
+        EchoServer::new_at_address(format!("127.0.0.1:{}", rng.random_range(1024..65535))).await
     }
 
     pub async fn new_at_address(bind_addr_string: String) -> EchoServer {
-        let bind_addr = bind_addr_string.parse().unwrap();
-        // Create a one-shot channel that can be used to tell the server to shut down
-        let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
-
-        // Start a separate server task
+        let bind_addr_string_clone = bind_addr_string.clone(); // Clone for the closure
+        let (shutdown_tx, mut shutdown_rx) = oneshot::channel::<()>();
         let server_state = Arc::new(ServerState {
             requests_received: atomic::AtomicUsize::new(0),
         });
         let server_task_state = server_state.clone();
         let server_task = tokio::spawn(async move {
-            let service = make_service_fn(|_| {
-                let server_task_state = server_task_state.clone();
-                async move {
-                    Ok::<_, hyper::Error>(service_fn(move |req| {
-                        let server_task_state = server_task_state.clone();
-                        echo(server_task_state, req)
-                    }))
+            let listener = tokio::net::TcpListener::bind(&bind_addr_string_clone)
+                .await
+                .unwrap();
+            loop {
+                tokio::select! {
+                    _ = &mut shutdown_rx => {
+                        break;
+                    }
+                    result = listener.accept() => {
+                        let (stream, _) = result.unwrap();
+                        let io = hyper_util::rt::TokioIo::new(stream);
+                        let service = service_fn({
+                            let server_task_state = server_task_state.clone();
+                            move |req| {
+                                let server_task_state = server_task_state.clone();
+                                echo(server_task_state, req)
+                            }
+                        });
+                        tokio::spawn(async move {
+                            if let Err(e) = hyper::server::conn::http1::Builder::new()
+                                .serve_connection(io, service)
+                                .await
+                            {
+                                log::error!("Error serving connection: {}", e);
+                            }
+                        });
+                    }
                 }
-            });
-            let server = hyper::Server::bind(&bind_addr)
-                .serve(service)
-                .with_graceful_shutdown(async {
-                    shutdown_rx.await.ok();
-                });
-            // Start serving and wait for the server to exit
-            if let Err(e) = server.await {
-                log::error!("Error in EchoServer: {}", e);
             }
         });
-
         EchoServer {
             shutdown_signal_sender: shutdown_tx,
             server_task,
             state: server_state,
-            address: bind_addr_string,
+            address: bind_addr_string, // Use the original
         }
     }
 }
@@ -88,13 +100,10 @@ impl EchoServer {
 #[async_trait]
 impl Server for EchoServer {
     async fn stop(self: Box<Self>) -> usize {
-        // Tell the hyper server to stop
         let _ = self.shutdown_signal_sender.send(());
-        // Wait for it to stop
         self.server_task
             .await
-            .expect("ErrorServer server task panicked");
-
+            .expect("EchoServer server task panicked");
         self.state.requests_received.load(atomic::Ordering::SeqCst)
     }
 
--- proj-2/balancebeam/tests/common/error_server.rs
+++ proj-2/balancebeam/tests/common/error_server.rs
@@ -1,72 +1,84 @@
 use crate::common::server::Server;
 use async_trait::async_trait;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Response};
+use http_body_util::Empty;
+use hyper::body::Bytes;
+use hyper::service::service_fn;
+use hyper::Response;
 use rand::Rng;
 use std::sync::{atomic, Arc};
 use tokio::sync::oneshot;
 
+/// Represents the state of the server including the count of received requests.
 #[derive(Debug)]
 struct ServerState {
+    /// Atomic counter for the number of requests received.
     pub requests_received: atomic::AtomicUsize,
 }
 
-#[allow(dead_code)]
-async fn return_error() -> Result<Response<Body>, hyper::Error> {
+/// Returns an HTTP response with a 500 Internal Server Error status.
+async fn return_error() -> Result<Response<Empty<Bytes>>, hyper::Error> {
     Ok(Response::builder()
         .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-        .body(Body::empty())
+        .body(Empty::new())
         .unwrap())
 }
 
+/// A server that always returns an internal server error for any request.
 pub struct ErrorServer {
     shutdown_signal_sender: oneshot::Sender<()>,
     server_task: tokio::task::JoinHandle<()>,
+    /// The address on which the server is listening.
     pub address: String,
     state: Arc<ServerState>,
 }
 
 impl ErrorServer {
-    #[allow(dead_code)]
+    /// Creates a new `ErrorServer` instance binding to a random available port.
     pub async fn new() -> ErrorServer {
-        let mut rng = rand::thread_rng();
-        ErrorServer::new_at_address(format!("127.0.0.1:{}", rng.gen_range(1024, 65535))).await
+        let mut rng = rand::rng();
+        ErrorServer::new_at_address(format!("127.0.0.1:{}", rng.random_range(1024..65535))).await
     }
 
-    #[allow(dead_code)]
+    /// Creates a new `ErrorServer` instance binding to the specified address.
     pub async fn new_at_address(bind_addr_string: String) -> ErrorServer {
-        let bind_addr = bind_addr_string.parse().unwrap();
-        // Create a one-shot channel that can be used to tell the server to shut down
-        let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
-
-        // Start a separate server task
+        let bind_addr_string_clone = bind_addr_string.clone();
+        let (shutdown_tx, mut shutdown_rx) = oneshot::channel::<()>();
         let server_state = Arc::new(ServerState {
             requests_received: atomic::AtomicUsize::new(0),
         });
+        // Clone the state to share it between connections.
         let server_task_state = server_state.clone();
         let server_task = tokio::spawn(async move {
-            let service = make_service_fn(|_| {
-                let server_task_state = server_task_state.clone();
-                async move {
-                    Ok::<_, hyper::Error>(service_fn(move |_req| {
-                        server_task_state
-                            .requests_received
-                            .fetch_add(1, atomic::Ordering::SeqCst);
-                        return_error()
-                    }))
+            let listener = tokio::net::TcpListener::bind(&bind_addr_string_clone)
+                .await
+                .unwrap();
+            loop {
+                tokio::select! {
+                    _ = &mut shutdown_rx => {
+                        break;
+                    }
+                    result = listener.accept() => {
+                        let (stream, _) = result.unwrap();
+                        let io = hyper_util::rt::TokioIo::new(stream);
+                        // Clone the shared state for this connection.
+                        let state = server_task_state.clone();
+                        let service = service_fn(move |_req| {
+                            // Increment the request counter atomically.
+                            state.requests_received.fetch_add(1, atomic::Ordering::SeqCst);
+                            return_error()
+                        });
+                        tokio::spawn(async move {
+                            if let Err(e) = hyper::server::conn::http1::Builder::new()
+                                .serve_connection(io, service)
+                                .await
+                            {
+                                log::error!("Error serving connection: {}", e);
+                            }
+                        });
+                    }
                 }
-            });
-            let server = hyper::Server::bind(&bind_addr)
-                .serve(service)
-                .with_graceful_shutdown(async {
-                    shutdown_rx.await.ok();
-                });
-            // Start serving and wait for the server to exit
-            if let Err(e) = server.await {
-                log::error!("Error in ErrorServer: {}", e);
             }
         });
-
         ErrorServer {
             shutdown_signal_sender: shutdown_tx,
             server_task,
@@ -78,17 +90,16 @@ impl ErrorServer {
 
 #[async_trait]
 impl Server for ErrorServer {
+    /// Stops the server and returns the number of requests received.
     async fn stop(self: Box<Self>) -> usize {
-        // Tell the hyper server to stop
         let _ = self.shutdown_signal_sender.send(());
-        // Wait for it to stop
         self.server_task
             .await
             .expect("ErrorServer server task panicked");
-
         self.state.requests_received.load(atomic::Ordering::SeqCst)
     }
 
+    /// Returns the address the server is bound to.
     fn address(&self) -> String {
         self.address.clone()
     }
